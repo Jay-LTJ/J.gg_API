f93539e0bdc923ab09d2f63df7688d5d3ac80e51
Jay-LTJ <115566953+Jay-LTJ@users.noreply.github.com>
2026-05-13 00:08:37 +0100

    simple LolData class done championDOA

--- src/main/java/com/example/J/gg_API/LolStaticDataConnection.java
+++ src/main/java/com/example/J/gg_API/LolStaticDataConnection.java
@@ -1,4 +1,19 @@
 package com.example.J.gg_API;
 
 public class LolStaticDataConnection {
+
+    private String link;
+
+    public LolStaticDataConnection() {
+        link = "https://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/";
+    }
+
+    public String getChampionLink(){
+        return link + "champions";
+    }
+
+    public String getItemLink(){
+        return link + "items";
+    }
+
 }
